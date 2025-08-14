da589b1b650bc76b3f6274bcf84c0975726267de
Njioda <70759040+Njioda@users.noreply.github.com>
2025-08-14 18:25:28 +0200

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -5,11 +5,19 @@ module.exports = defineConfig({
   viewportWidth: 1920,
   video: false,
   e2e: {
+    setupNodeEvents(on, config) {
+      // config for test reporter
+    },
     baseUrl: "https://demoblaze.com/",
     excludeSpecPattern: ["**/1-getting-started", "**/2-advanced-examples"],
     specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx}",
     setupNodeEvents(on, config) {
       // implement node event listeners here
+    },
+    reporter: "mocha-junit-reporter",
+    reporterOptions: {
+      mochaFile: "cypress/results/results-[hash].xml",
+      toConsole: true
     }
   }
 });
